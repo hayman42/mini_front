bfc57b7a36a9e48e89e78b8cb45563113a0ff87e
vcho1958 <gusckd57@gmail.com>
2022-04-30 21:44:42 +0900

    [feat]자기소개 모달창 추가

---
+++ src/mock.js
@@ -0,0 +1,34 @@
+export const mock = [
+    {
+        id: 1,
+        user: 1,
+        title: `자기소개1`,
+        text: `
+  asdasdqweqwe
+  # asd
+  ## asd
+  ### asd
+  #### asd`,
+    },
+    {
+        id: 2,
+        user: 1,
+        title: `자기소개2`,
+        text: `
+  asdasdqweqweasdasd`,
+    },
+    {
+        id: 3,
+        userId: 2,
+        title: `자기소개1`,
+        text: `
+  asdasdqweqweasdasd`,
+    },
+    {
+        id: 4,
+        userId: 2,
+        title: `자기소개2`,
+        text: `
+  asdasdqweqweasdasd`,
+    },
+];
