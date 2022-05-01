fd7d3f1d15dfd43412d324b2a2ed1189395fba16
박재철 <sis3594@gmail.com>
2022-05-01 18:04:10 +0900

    [feat] mainpage

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,81 @@
 <script>
-	export let name;
+	export let username = "name";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<header>
+		<div style="height: 20px"></div>
+		<ul class="nav justify-content-center">
+			<li class="nav-item">
+				<div>
+					<a class="nav-link active" aria-current="page" href="#"><img src="/logo1.png" alt="logo"></a>
+				</div>
+			</li>
+			<li class="nav-item">
+				<div>
+					<p>Welcome!<br>{username}</p>
+				</div>
+			</li>
+			<li class="nav-item">
+				<div>
+					<a class="nav-link" href="#">Sign Out</a>
+				</div>
+			</li>
+			<li class="nav-item">
+				<div>
+			  		<a class="nav-link" href="#">See Others</a>
+				</div>
+			</li>
+		  </ul>
+	</header>
+	<div style='height: 100px'></div>
+	<img src="/logo1.png" alt="logo">
+	<div style='height: 50px'></div>
+	<button type="button" class="btn btn-link">
+		<a href="#" style="color: #E3DCD2"> Sign In / Sign Up</a>
+	</button>
 </main>
 
 <style>
+	header {
+		float: left;
+		height: 100%;
+		width: 100px;
+		background-color: #013328;
+	}
+
+	header ul li div {
+		margin: 10px;
+		background-color: #E3DCD2;
+		border-radius: 5%;
+	}
+	header ul li div a {
+		color: #100C0D;
+	}
+
+	header ul li a img {
+		height: 50px;
+		width: auto;
+		padding: 10px 10px;
+	}
+
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		background-color: #CC8B65;
+		width: 100%;
+		height: 100%;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	button {
+		height: 40px;
+		background-color: #013328;
+		border-radius: 10%;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	img {
+		display: block;
+		margin: auto;
+		width:200px;
+		height: auto;
 	}
 </style>
