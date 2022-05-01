f75c56616926f7c4d2d741115b6704382d9120ec
한재연 <hayman42@naver.com>
2022-05-01 16:08:29 +0900

    [feat]초기 메인화면 구성

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,37 @@
 <script>
 	import Signin from "./Signin.svelte";
-	function isLoggedIn() {
-		console.log(document.cookie.includes("userid="));
-		return document.cookie.includes("userid=");
-	}
+	import Userinfo from "./Userinfo.svelte";
+	// import Myintro from "./Myintro.svelte";
+	import {
+		Image,
+		Button,
+		Modal,
+		ModalHeader,
+		ModalBody,
+		ModalFooter,
+		Nav,
+		NavItem,
+		Navbar,
+	} from "sveltestrap";
+
+	export let serverUrl;
+	let open = false;
+	let isLoggedIn = false;
+	const toggle = () => (open = !open);
 </script>
 
 <main>
-	{#if isLoggedIn()}
-		<p>Like</p>
-	{:else}
-		<Signin />
-	{/if}
+	<!-- {#if isLoggedIn} -->
+	<!-- <Myintro {serverUrl} /> -->
+	<!-- {:else} -->
+	<Image alt="Landscape" src="./logo.png" style="width:25%;height:25%" />
+	<br />
+	<Button on:click={toggle}>Get Started</Button>
+	<Modal isOpen={open} {toggle}>
+		<ModalHeader {toggle}>로그인을 하여 시작하세요.</ModalHeader>
+		<ModalBody>
+			<Signin {serverUrl} bind:isLoggedIn />
+		</ModalBody>
+	</Modal>
+	<!-- {/if} -->
 </main>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import App from './App.svelte';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 const app = new App({
-	target: document.body
+	target: document.body,
+	props: {
+		serverUrl: "http://localhost:5000"
+	}
 });
 
 export default app;
